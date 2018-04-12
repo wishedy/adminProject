c844adb37e59ee6e4d9661757bf164f2012b7000
张恒 <zhangheng@allinmd.cn>
2018-04-12 09:27:52 +0800

    项目创建

---
+++ README.md
@@ -0,0 +1,7 @@
+# vue2-vue-router2-webpack3
+
+## Blog
+《从零搭建vue2+vue-router2+webpack3工程》
+
+[http://www.qinshenxue.com/article/20161118151423.html](http://www.qinshenxue.com/article/20161118151423.html)
+
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './app.vue';
+import routerConfig from './router';
+Vue.use(VueRouter);
+var router = new VueRouter(routerConfig)
+import './css/base.css'
+new Vue({
+    el: '#app',
+    router: router,
+    render: h => h(App)
+});
